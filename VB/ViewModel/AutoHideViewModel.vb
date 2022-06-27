05e445c3ea8c09ffd7ef7b036b162a2c310a628b
DevExpressExampleBot <fakeEmail>
2022-06-27 11:43:51 +0400

    Source auto update [skip ci]

--- VB/ViewModel/AutoHideViewModel.vb
+++ VB/ViewModel/AutoHideViewModel.vb
@@ -1,42 +1,27 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports DevExpress.Xpf.Docking
 
 Namespace WpfApplication
-	Public Class AutoHideViewModel
-		Implements IMVVMDockingProperties
-		Public Sub New(ByVal caption As String)
-			Caption = caption
-			MyTargetName = "auotHideGroup"
-		End Sub
-		Private privateCaption As String
-		Public Property Caption() As String
-			Get
-				Return privateCaption
-			End Get
-			Set(ByVal value As String)
-				privateCaption = value
-			End Set
-		End Property
-		Private privateMyTargetName As String
-		Public Property MyTargetName() As String
-			Get
-				Return privateMyTargetName
-			End Get
-			Set(ByVal value As String)
-				privateMyTargetName = value
-			End Set
-		End Property
-        Private Property TargetName() As String Implements IMVVMDockingProperties.TargetName
+
+    Public Class AutoHideViewModel
+        Implements IMVVMDockingProperties
+
+        Public Sub New(ByVal caption As String)
+            Me.Caption = caption
+            MyTargetName = "auotHideGroup"
+        End Sub
+
+        Public Property Caption As String
+
+        Public Property MyTargetName As String
+
+        Private Property TargetName As String Implements IMVVMDockingProperties.TargetName
             Get
                 Return MyTargetName
             End Get
+
             Set(ByVal value As String)
                 MyTargetName = value
             End Set
         End Property
-	End Class
+    End Class
 End Namespace
--- VB/ViewModel/PanelViewModel.vb
+++ VB/ViewModel/PanelViewModel.vb
@@ -1,47 +1,40 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports DevExpress.Xpf.Docking
 Imports System.Windows.Media
 
 Namespace WpfApplication
-	Public Class PanelViewModel
+
+    Public Class PanelViewModel
         Implements IMVVMDockingProperties
-		Private privateCaption As String
-		Public Property Caption() As String
-			Get
-				Return privateCaption
-			End Get
-			Set(ByVal value As String)
-				privateCaption = value
-			End Set
-		End Property
-		Public Sub New(ByVal c As Color, ByVal caption As String)
-			_Color = New SolidColorBrush(c)
-			Caption = caption
-		End Sub
 
-		Private Property TargetName() As String Implements IMVVMDockingProperties.TargetName
-			Get
-				Return "DockPanels"
-			End Get
-			Set(ByVal value As String)
-				Throw New NotImplementedException()
-			End Set
-		End Property
+        Public Property Caption As String
 
-		' Fields...
-		Private _Color As SolidColorBrush
+        Public Sub New(ByVal c As Color, ByVal caption As String)
+            _Color = New SolidColorBrush(c)
+            Me.Caption = caption
+        End Sub
 
+        Private Property TargetName As String Implements IMVVMDockingProperties.TargetName
+            Get
+                Return "DockPanels"
+            End Get
 
-		Public Property ColorBrush() As SolidColorBrush
-			Get
-				Return _Color
-			End Get
-			Set(ByVal value As SolidColorBrush)
-				_Color = value
-			End Set
-		End Property
-	End Class
+            Set(ByVal value As String)
+                Throw New NotImplementedException()
+            End Set
+        End Property
+
+        ' Fields...
+        Private _Color As SolidColorBrush
+
+        Public Property ColorBrush As SolidColorBrush
+            Get
+                Return _Color
+            End Get
+
+            Set(ByVal value As SolidColorBrush)
+                _Color = value
+            End Set
+        End Property
+    End Class
 End Namespace
