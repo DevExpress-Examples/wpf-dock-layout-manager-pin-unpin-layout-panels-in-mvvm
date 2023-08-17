5713865a572cf867723d255bd7a715f7c608f4ce
DevExpressExampleBot <fakeEmail>
2023-08-17 16:25:48 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
 Imports System.Windows
-Imports System.Windows.Controls
 
 Namespace WpfApplication
 
@@ -11,24 +10,6 @@ Namespace WpfApplication
 
         Public Sub New()
             Me.InitializeComponent()
-            vm = TryCast(Me.mainGrid.Resources("viewModel"), MainViewModel)
-            ahvm = TryCast(vm.ChildViews(7), AutoHideViewModel)
-        End Sub
-
-        Private vm As MainViewModel
-
-        Private ahvm As AutoHideViewModel
-
-        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            vm.ChildViews.Remove(ahvm)
-            ahvm.MyTargetName = "DockPanels"
-            vm.ChildViews.Add(ahvm)
-        End Sub
-
-        Private Sub Button_Click_1(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            vm.ChildViews.Remove(ahvm)
-            ahvm.MyTargetName = "auotHideGroup"
-            vm.ChildViews.Add(ahvm)
         End Sub
     End Class
 End Namespace
--- VB/ViewModel/MainViewModel.vb
+++ VB/ViewModel/MainViewModel.vb
@@ -1,9 +1,12 @@
+Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.DataAnnotations
 Imports System.Collections.ObjectModel
 Imports System.Windows.Media
 
 Namespace WpfApplication
 
     Public Class MainViewModel
+        Inherits ViewModelBase
 
         Public Sub New()
             ChildViews.Add(New DocumentViewModel("Document1", "/Images/change.png"))
@@ -13,8 +16,11 @@ Namespace WpfApplication
             ChildViews.Add(New PanelViewModel(Colors.Blue, "2"))
             ChildViews.Add(New PanelViewModel(Colors.Green, "3"))
             ChildViews.Add(New AutoHideViewModel("Properties"))
-            ChildViews.Add(New AutoHideViewModel("Messages"))
+            MessagesViewModel = New AutoHideViewModel("Messages")
+            ChildViews.Add(MessagesViewModel)
         End Sub
+
+        Private Property MessagesViewModel As AutoHideViewModel
 
         Private _Children As ObservableCollection(Of Object) = New ObservableCollection(Of Object)()
 
@@ -27,5 +33,25 @@ Namespace WpfApplication
                 _Children = value
             End Set
         End Property
+
+        Private Sub MoveMessagesViewModel(ByVal targetGroupName As String)
+            ChildViews.Remove(MessagesViewModel)
+            MessagesViewModel.MyTargetName = targetGroupName
+            ChildViews.Add(MessagesViewModel)
+        End Sub
+
+        <Command(CanExecuteMethodName:="CanMoveMessages")>
+        Public Sub PinMessages()
+            MoveMessagesViewModel("DockPanels")
+        End Sub
+
+        Public Function CanMoveMessages() As Boolean
+            Return ChildViews.Contains(MessagesViewModel)
+        End Function
+
+        <Command(CanExecuteMethodName:="CanMoveMessages")>
+        Public Sub UnpinMessages()
+            MoveMessagesViewModel("auotHideGroup")
+        End Sub
     End Class
 End Namespace
